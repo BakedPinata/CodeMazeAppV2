911ab2e82b439a435adedc8210297a3e60f2238a
MichaelArcher <Michael.Archer@luerssen.com.au>
2024-10-11 08:21:50 +0800

    1.3 Program.cs Class Explanations

--- src/CompanyEmployees/Program.cs
+++ src/CompanyEmployees/Program.cs
@@ -1,17 +1,41 @@
+﻿/// <summary>
+/// A WebApplicationBuilder that is responsible for:
+/// - Adding Configuration to the project by using the builder.Configuration property
+/// - Registering services in our app with the builder.Services property
+/// - Logging configuration with the builder.Logging property
+/// - Other IHostBuilder and IWebHostBuilder configuration
+/// </summary>
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+/// <summary>
+/// Add services to the container.
+/// A service is a reusable part of the code that adds some functionality to our application.
+/// </summary>
+#region Register Services
 
 builder.Services.AddControllers();
 
+#endregion
+
+
+/// <summary>
+/// A WebApplication we can use to add middleware components to the application's request
+/// pipeline.
+/// </summary>
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/// <summary>
+/// Configure the HTTP request pipeline.
+/// </summary>
+#region Configure Middleware
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
 
 app.Run();

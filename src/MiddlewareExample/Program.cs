08b164fc35d0dbea82a1f0ead9e5287ef0e7d1dc
MichaelArcher <Michael.Archer@luerssen.com.au>
2024-10-11 12:38:31 +0800

    1.8.1 Creating the First Middleware Component

---
+++ src/MiddlewareExample/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Example middleware that assigns an anonymous method to the Run method. Since the Run method is
+// always terminal (end), any component added with this method will terminate the pipeline.
+// This is why the response from the controller is not returned.
+app.Run(async context =>
+{
+  await context.Response.WriteAsync("Hello from the middleware component.");
+});
+
+app.MapControllers();
+
+app.Run();
